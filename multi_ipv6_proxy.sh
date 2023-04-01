c221bbc3807887f63ce878881229b3771739d6f3
hedemele47 <129479671+hedemele47@users.noreply.github.com>
2023-04-01 05:24:29 +0300

    Update multi_ipv6_proxy.sh

--- multi_ipv6_proxy.sh
+++ multi_ipv6_proxy.sh
@@ -14,21 +14,21 @@ gen64() {
 }
 
 
-#
-#install_3proxy() {
-#    echo "installing 3proxy"
-#    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
-#    wget -qO- $URL | bsdtar -xvf-
-#    cd 3proxy-3proxy-0.8.6
-#    make -f Makefile.Linux
-#    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-#    cp src/3proxy /usr/local/etc/3proxy/bin/
-#    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-#    chmod +x /etc/init.d/3proxy
-#    chkconfig 3proxy on
-#    cd $WORKDIR
-#}
-#
+
+install_3proxy() {
+   echo "installing 3proxy"
+    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
+   wget -qO- $URL | bsdtar -xvf-
+ cd 3proxy-3proxy-0.8.6
+  make -f Makefile.Linux
+ mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+   cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+   chkconfig 3proxy on
+    cd $WORKDIR
+}
+
 
 gen_3proxy() {
     cat <<EOF
